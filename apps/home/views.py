cb87f9e0c0e46e1f84a8154668b049918be8be03
[username] <[email]>
2018-09-17 19:53:50 +0800

    demo16

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from apps.home.models import *
 
 def index(request):
-    return HttpResponse("首页")
+    navigations=Navigation.objects.all()
+    categories=Category.objects.all()
+    for category in categories:
+        categories.subs=category.submenu_set.all()
+        for sub in category.subs:
+            sub.subs2=sub.submenu2_set.all()
+    banners=Banner.objects.all()
+    return render(request,'index.html',{'navigations':navigations,'banners':banners})
+
 # Create your views here.
